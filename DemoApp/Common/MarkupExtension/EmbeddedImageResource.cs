63713009e49ecbb079b8cace3b99df19a8c1a2f6
truonglam5555 <truonglam5555@gmail.com>
2021-11-03 14:21:46 +0700

    Add code

---
+++ DemoApp/Common/MarkupExtension/EmbeddedImageResource.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace DemoApp.Common.MarkupExtension
+{
+    [Preserve(AllMembers = true)]
+    [ContentProperty(nameof(SourceImage))]
+    public class EmbeddedImageResource : IMarkupExtension
+    {
+        public string SourceImage { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (SourceImage == null)
+                return null;
+            SourceImage = "Vitaorga.Resources.Images." + SourceImage;
+            // Do your translation lookup here, using whatever method you require
+            var imageSource = ImageSource.FromResource(SourceImage, typeof(EmbeddedImageResource).GetTypeInfo().Assembly);
+
+            return imageSource;
+        }
+    }
+
+    public class EmbeddedImageResourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var Source = "Vitaorga.Resources.Images." + value.ToString();
+            return ImageSource.FromResource(Source, typeof(EmbeddedImageResource).GetTypeInfo().Assembly);
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
